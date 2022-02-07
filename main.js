ba9dd394344a4cd90cd7f7ff35f635c695480085
quayum quayum <96136071+abdulquayumhp@users.noreply.github.com>
2022-02-07 16:47:01 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// navbar er jonn js used 
+// navbar er jonn js used 
+// navbar er jonn js used 
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+ 
+  function closeNav() {
+   document.getElementById("mySidepanel").style.width = "0";
+   setTimeout =(1000);
+ }
